599c63c47b297c8060f9e5f2f0c2d80269c0362a
iwfan <i.wangfancn@gmail.com>
2023-01-05 15:38:37 +0800

    update bot

--- main.ts
+++ main.ts
@@ -1,28 +1,46 @@
-import * as grammy from "https://deno.land/x/grammy@v1.13.0/mod.ts"
-
-declare interface ExecutionContext {
-  waitUntil(promise: void | Promise<void>): void;
-  passThroughOnException(): void;
-}
-
 export interface Env {
-  // Example binding to KV. Learn more at https://developers.cloudflare.com/workers/runtime-apis/kv/
-  // MY_KV_NAMESPACE: KVNamespace;
-  //
-  // Example binding to Durable Object. Learn more at https://developers.cloudflare.com/workers/runtime-apis/durable-objects/
-  // MY_DURABLE_OBJECT: DurableObjectNamespace;
-  //
-  // Example binding to R2. Learn more at https://developers.cloudflare.com/workers/runtime-apis/r2/
-  // MY_BUCKET: R2Bucket;
+  TELEGRAM_BOT_TOKEN: string;
+  TELEGRAM_CHAT_ID: string;
 }
 
-
-export default {
-  fetch(request: Request, env: Env, ctx: ExecutionContext): Response {
-    return new Response("Hello, world! Wrangler + Deno");
-  },
+export default (request: any) => {
+  const message = request.body?.message
+  console.log("REQUEST METHOD", request.method)
+  console.log("REQUEST BODY", request.body)
+  return new Response(`Hello, from Deno v${Deno.version.deno}! ${JSON.stringify(message)}`);
 };
 
+/* export default { */
+/*   async fetch( */
+/*     request: Request, */
+/*   ): Promise<Response> { */
+/*     const { message } = await request.json(); */
+/**/
+/*     console.log(JSON.stringify(message)) */
+/**/
+/*     if (message) { */
+/*       const { chat: { id, first_name }, text, message_id } = message; */
+/**/
+/*       await fetch( */
+/*         `https://api.telegram.org/bot${env.TELEGRAM_BOT_TOKEN}/sendMessage`, */
+/*         { */
+/*           method: "POST", */
+/*           cache: "no-cache", */
+/*           credentials: "omit", */
+/*           redirect: "follow", */
+/*           referrerPolicy: "no-referrer", */
+/*           body: JSON.stringify({ */
+/*             chat_id: env.TELEGRAM_CHAT_ID, */
+/*             text: `Echo: ${message}`, */
+/*           }), */
+/*         }, */
+/*       ); */
+/*     } */
+/**/
+/*     return new Response("Hello, world! Wrangler + Deno"); */
+/*   }, */
+/* }; */
+
 if (import.meta.main) {
-  console.log("[^_^]")
+  console.log("[^_^]");
 }
