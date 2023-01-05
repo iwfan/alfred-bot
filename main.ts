6f6ac3b5dc9beddb0fb42e1be1c98f8f23c0bd1e
iwfan <i.wangfancn@gmail.com>
2023-01-05 22:41:08 +0800

    update

--- main.ts
+++ main.ts
@@ -7,6 +7,7 @@ export default (request: any) => {
   const message = request.body?.message
   console.log("REQUEST METHOD", request.method)
   console.log("REQUEST BODY", request.body)
+  console.log("11")
   return new Response(`Hello, from Deno v${Deno.version.deno}! ${JSON.stringify(message)}`);
 };
 
